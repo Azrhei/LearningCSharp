28240836fc0734e58a64d8d9b54c43a7f19bc70f
Azrhei <azrheilp@gmail.com>
2024-04-10 13:32:40 -0500

    First Version - Hello World!

---
+++ Learning CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Learning_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return;
+
+        }
+    }
+}
